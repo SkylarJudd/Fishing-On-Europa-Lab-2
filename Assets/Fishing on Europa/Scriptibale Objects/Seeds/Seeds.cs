48a38dfa534e498888074d92618a0055c304d4e6
Valgius <jax.skinner@hotmail.com>
2024-06-22 13:09:02 +1000

    Farming: Scriptable objects

--- Assets/Fishing on Europa/Scriptibale Objects/Seeds/Seeds.cs
+++ Assets/Fishing on Europa/Scriptibale Objects/Seeds/Seeds.cs
@@ -8,7 +8,18 @@ public class Seeds : ScriptableObject
 {
     public string seedName;
     public string seedDesc;
-    public int spawnLocation; // 1 = Lake, 2 = River, 3 = Pond
-    public float growTime;
     public string foodProduced;
+
+    public int spawnLocation; // 1 = Lake, 2 = River, 3 = Pond
+    public float growthTime;
+    public int price;
+
+
+    public Sprite artwork;
+
+    [Header("Plant Stages")]
+    public GameObject seedling;
+    public GameObject sprout;
+    public GameObject adolecent;
+    public GameObject mature;
 }
