9520d3ce188172677a9346a7f294d2b61564f812
SkylarJudd <136395161+SkylarJudd@users.noreply.github.com>
2024-07-29 21:33:35 +1000

    FOE: updated Files

---
+++ Assets/_FishingOnEuropa/Scripts/FishIng Scripts/FishSpawnerGeyser.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static FishSpawnerGeyser;
+
+
+
+public class FishSpawnerGeyser : GameBehaviour
+{
+    [SerializeField] FishToSpawn fishToSpawn;
+    [SerializeField] Transform spawnPos;
+    [SerializeField] Transform targetPos;
+    [SerializeField] Transform randomLocation;
+
+    [SerializeField] List<HybridsToSpawn> fishList;
+
+    [Serializable]
+    public class HybridsToSpawn
+    {
+        public float spawnChance;
+        public float newSpawnChance;
+        public PoolType poolType;
+        public GameObject hybridGameObject;
+    }
+
+
+    [SerializeField] FishNavigationManager fishNavigationManager;
+    [SerializeField] GeyserMannager geyserMannager;
+
+
+
+    private void Start()
+    {
+
+        int total = 0;
+
+        for (int i = 0; i < fishToSpawn.FishToSpawnList.Count; i++)
+        {
+            HybridsToSpawn newEntry = new HybridsToSpawn();
+            newEntry.spawnChance = fishToSpawn.FishToSpawnList[i].spawnChance;
+            newEntry.hybridGameObject = fishToSpawn.FishToSpawnList[i].hybridPrefab;
+            newEntry.poolType = fishToSpawn.FishToSpawnList[i].poolType;
+            total += fishToSpawn.FishToSpawnList[i].spawnChance;
+            fishList.Add(newEntry);
+        }
+
+        //print(total);
+        //print(FishList[1].spawnChance / total);
+
+        for (int i = 0; i < fishToSpawn.FishToSpawnList.Count; i++)
+        {
+            fishList[i].newSpawnChance = (fishList[i].spawnChance / total) * 100;
+        }
+
+        fishList.Sort((x, y) => x.newSpawnChance.CompareTo(y.newSpawnChance));
+    }
+
+    public void spawnHybrid()
+    {
+
+        HybridsToSpawn hybridToSpawn = GetFishToSpawn();
+
+        Vector3 direction = targetPos.position - spawnPos.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+        //Debug.Log($"Spawning {hybridToSpawn.hybridGameObject.name} at {spawnPos.position} looking towards {targetPos.position} with rotation {lookRotation.eulerAngles}");
+        GameObject hybridFromManager = ObjectPoolManager._OPM.spawnObject(hybridToSpawn.hybridGameObject, spawnPos.transform.position, lookRotation, hybridToSpawn.poolType);
+        //Debug.Log($"Spawned object {hybridFromManager.name} with rotation {hybridFromManager.transform.rotation.eulerAngles}");
+
+        fishNavigationManager.addHybridToPondList(hybridFromManager);
+
+        MoveToTarget(hybridFromManager, GetRandomPositionAround( targetPos));
+
+    }
+
+    public void MoveToTarget(GameObject fishHybrid, Vector3 target)
+    {
+        Rigidbody _Rb = fishHybrid.GetComponent<Rigidbody>();
+        if (_Rb == null)
+        {
+            Debug.LogError("No Rigidbody found on the instantiated fish object.");
+            return;
+        }
+
+        Vector3 distance = target - fishHybrid.transform.position;
+
+        float height = distance.y;
+        Vector3 horizontalDistance = new Vector3(distance.x, 0, distance.z);
+
+        float g = Physics.gravity.y;
+        float Vy;
+        Vector3 VXZ;
+
+        if (height > 0)
+        {
+            // Target is above the spawn point
+            Vy = Mathf.Sqrt(-2 * g * height);
+            float timeToReachApex = Vy / -g;
+            float totalTime = timeToReachApex * 2; // Time to reach the target when height > 0
+            VXZ = horizontalDistance / totalTime;
+        }
+        else
+        {
+            // Target is below or at the same level as the spawn point
+            Vy = Mathf.Sqrt(2 * -g * -height);
+            float timeToReachTarget = Mathf.Sqrt(2 * -height / -g);
+            VXZ = horizontalDistance / timeToReachTarget;
+        }
+
+        // Check for NaN values
+        if (float.IsNaN(Vy) || float.IsNaN(VXZ.x) || float.IsNaN(VXZ.z))
+        {
+            Debug.LogError($"Calculated velocities contain NaN values: Vy={Vy}, VXZ={VXZ}");
+            return;
+        }
+
+        // Set the velocity of the Rigidbody
+        _Rb.velocity = new Vector3(VXZ.x, Vy, VXZ.z);
+        _Rb.useGravity = true;
+
+        // Apply rotational force
+        Vector3 rotationalForce = new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f)); // This will rotate the object around its Y-axis
+
+        _Rb.angularVelocity = rotationalForce;
+    }
+
+    private HybridsToSpawn GetFishToSpawn()
+    {
+        
+        if ( 100 == UnityEngine.Random.Range(0, 100))
+        {
+            //bool isShiney = true;
+        }
+
+        float randomNumber = UnityEngine.Random.Range(0, 100);
+        float currentChanceValue = 0;
+
+        foreach (HybridsToSpawn hybrids in fishList)
+        {
+            currentChanceValue += hybrids.newSpawnChance;
+
+            if (randomNumber <= currentChanceValue)
+            {
+                return hybrids;
+            }
+        }
+
+        Debug.LogError($"Hybrid was outside the random range with a value of {randomNumber}, spawning {fishList[1].hybridGameObject}");
+        return fishList[1];
+    }
+
+    public Vector3 GetRandomPositionAround(Transform centerTransform)
+    {
+        float radius = 5f;
+
+        float randomAngle = UnityEngine.Random.Range(0f, 360f);
+        float x = centerTransform.position.x + radius * Mathf.Cos(randomAngle * Mathf.Deg2Rad);
+        float z = centerTransform.position.z + radius * Mathf.Sin(randomAngle * Mathf.Deg2Rad);
+        float y = centerTransform.position.y;
+
+        randomLocation.transform.position = new Vector3(x, y, z);
+
+        return new Vector3(x, y, z);
+    }
+}
---
+++ Assets/_FishingOnEuropa/Scripts/_Main/GameBehaviour.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GameBehaviour : MonoBehaviour
+{
+    protected static GameMannager _GM { get { return GameMannager.instance; } }
+    protected static ObjectPoolManager _OPM{ get { return ObjectPoolManager.instance; } }
+    
+
+    public Transform getClosestEnermy(Transform _origin, List<GameObject> _objects)
+    {
+        if (_objects == null || _objects.Count == 0)
+            return null;
+
+        float distance = Mathf.Infinity;
+        Transform closest = null;
+
+        foreach (GameObject go in _objects)
+        {
+            float currentDistance = Vector3.Distance(_origin.transform.position, go.transform.position);
+            if (currentDistance < distance)
+            {
+                closest = go.transform;
+                distance = currentDistance;
+            }
+        }
+        return closest;
+    }
+
+   
+}
