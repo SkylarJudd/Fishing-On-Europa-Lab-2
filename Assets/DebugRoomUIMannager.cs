72455ea6c02ee7a753f7e0c0c6fe4ff6695b0e22
SkylarJudd <136395161+SkylarJudd@users.noreply.github.com>
2024-07-30 17:36:24 +1000

    FOE: Main Menu

---
+++ Assets/DebugRoomUIMannager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugRoomUIMannager : MonoBehaviour
+{
+    public void OnReturnToMenuButtonPress()
+    {
+        print("Button Pressed Menu");
+    }
+    public void OnReturnToFarmButtonPress()
+    {
+        print("Button Pressed Farm");
+    }
+    public void OnReturnToDomeButtonPress()
+    {
+        print("Button Pressed Dome");
+    }
+}
