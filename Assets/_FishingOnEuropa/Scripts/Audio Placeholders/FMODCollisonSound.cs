b0af67b98299c4d19581a380d3089931fe20f02c
Valgius <jax.skinner@hotmail.com>
2024-09-17 23:31:18 +1000

    FMOD: Duplicated and converted Collision Sound script to FMOD. Tested on BouncyBass Prefab.
    Old Audio source and collision script was removed from BouncyBass and replaced with new version. Works. Other prefabs remain unchanged.

--- Assets/_FishingOnEuropa/Scripts/Audio Placeholders/FMODCollisonSound.cs
+++ Assets/_FishingOnEuropa/Scripts/Audio Placeholders/FMODCollisonSound.cs
@@ -2,49 +2,54 @@ using Autohand;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 [HelpURL("https://app.gitbook.com/s/5zKO0EvOjzUDeT2aiFk3/auto-hand/extras/collision-sounds")]
-public class CollisionSound : MonoBehaviour{
+public class CollisionSound : MonoBehaviour
+{
 
     [Tooltip("The layers that cause the sound to play")]
     public LayerMask collisionTriggers = ~0;
     [Tooltip("Source to play sound from")]
-    public AudioSource source;
-    [Tooltip("Source to play sound from")]
-    public AudioClip clip;
+    public StudioEventEmitter source;
+
     [Space]
     [Tooltip("Source to play sound from")]
     public AnimationCurve velocityVolumeCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
-    public float volumeAmp = 0.8f;
-    public float velocityAmp = 0.5f;
     public float soundRepeatDelay = 0.2f;
 
     Rigidbody body;
     bool canPlaySound = true;
     Coroutine playSoundRoutine;
 
-    private void Start() {
+    private void Start()
+    {
         body = GetComponent<Rigidbody>();
 
         //So the sound doesn't play when falling in place on start
         StartCoroutine(SoundPlayBuffer(1f));
     }
 
-    private void OnDisable(){
+    private void OnDisable()
+    {
         if (playSoundRoutine != null)
             StopCoroutine(playSoundRoutine);
     }
 
-    void OnCollisionEnter(Collision collision) {
+    void OnCollisionEnter(Collision collision)
+    {
         if (body == null && !gameObject.CanGetComponent(out body))
             return;
 
-        if(canPlaySound && collisionTriggers == (collisionTriggers | (1 << collision.gameObject.layer))) {
-            if(source != null && source.enabled){
-                if (collision.collider.attachedRigidbody == null || collision.collider.attachedRigidbody.mass > 0.0000001f){
-                    if(clip != null || source.clip != null)
-                        source.PlayOneShot(clip == null ? source.clip : clip, velocityVolumeCurve.Evaluate(collision.relativeVelocity.magnitude * velocityAmp) * volumeAmp);
+        if (canPlaySound && collisionTriggers == (collisionTriggers | (1 << collision.gameObject.layer)))
+        {
+            if (source != null && source.enabled)
+            {
+                if (collision.collider.attachedRigidbody == null || collision.collider.attachedRigidbody.mass > 0.0000001f)
+                {
+                    if (source!= null)
+                        source.Play();
                     if (playSoundRoutine != null)
                         StopCoroutine(playSoundRoutine);
                     playSoundRoutine = StartCoroutine(SoundPlayBuffer());
@@ -53,14 +58,16 @@ public class CollisionSound : MonoBehaviour{
         }
     }
 
-    IEnumerator SoundPlayBuffer() {
+    IEnumerator SoundPlayBuffer()
+    {
         canPlaySound = false;
         yield return new WaitForSeconds(soundRepeatDelay);
         canPlaySound = true;
         playSoundRoutine = null;
     }
 
-    IEnumerator SoundPlayBuffer(float time) {
+    IEnumerator SoundPlayBuffer(float time)
+    {
         canPlaySound = false;
         yield return new WaitForSeconds(time);
         canPlaySound = true;
